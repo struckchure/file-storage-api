61a129332010b6fb6caf1e30e369caba7b04a64e
Joshua <cutejosh2@gmail.com>
2022-11-09 17:11:03 +0100

    added feat for swagger docs

--- file_storage/settings.py
+++ file_storage/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from datetime import timedelta
 import dotenv
@@ -130,6 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'file_storage/static'),
+]
 
 # Media files
 MEDIA_URL = "media/"
